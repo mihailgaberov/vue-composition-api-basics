3985116dce98a5a3d750b789a217212488972763
mihailgaberov <mihail.gaberov@gmail.com>
2024-07-06 15:24:17 +0200

    :student: teleport - part 1

---
+++ src/views/ModalsView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="modals">
+        <h1>Modals</h1>
+
+        <button @click="showModal = true">Show modal</button>
+        <div class="modal" v-if="showModal">
+            <h1>This is modal</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, quos molestiae et repudiandae enim
+                officia eligendi illo minus tempore quibusdam quaerat corporis ipsa error dolor nesciunt voluptas
+                possimus similique quidem?</p>
+            <button @click="showModal = false">Hide modal</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const showModal = ref(false)
+
+</script>
+
+<style :scope>
+.modal {
+    background-color: beige;
+    color: black;
+    padding: 1rem;
+}
+</style>
