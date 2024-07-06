35450220153c502eb73d3df9fbb81c2b5d60e5ec
mihailgaberov <mihail.gaberov@gmail.com>
2024-07-06 13:01:18 +0200

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Abouty</h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <h3>{{ counterTitle }}</h3>
+    <div>
+      <button @click="decrement" class="btn">-</button>
+      <span class="counter">{{ count }}</span>
+      <button @click="increment" class="btn">+</button>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0), counterTitle = ref('My Counter');
+const increment = () => count.value++
+const decrement = () => count.value--
+</script>
+
+<style>
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.btn,
+.counter {
+  font-size: 2rem;
+  padding: 0.5rem 1rem;
+}
+</style>
