bd319a4189ff7a88e4557e6b7f6e22bfc1908796
mihailgaberov <mihail.gaberov@gmail.com>
2024-07-06 14:02:39 +0200

    :student: $route part 2

---
+++ src/views/PostDetailView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="post-detail">
+    <h1>This is a post page</h1>
+
+    <p>Display the content of the post with ID of {{ $route.params.id }} here.</p>
+
+    <p>
+      <RouterLink to="/posts">&lt; Back</RouterLink>
+    </p>
+  </div>
+</template>
+<script setup>
+
+</script>
---
+++ src/views/PostsView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="posts">
+    <h1>Posts</h1>
+
+    <ul>
+      <li>
+        <RouterLink to="/postDetail/1">Post 1</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/postDetail/2">Post 2</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/postDetail/3">Post 3</RouterLink>
+      </li>
+    </ul>
+
+    <textarea v-auto-focus></textarea>
+  </div>
+</template>
+
+
+<script setup>
+import { vAutoFocus } from '@/directives/vAutoFocus';
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin-bottom: 39px;
+}
+</style>
